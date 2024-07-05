383d764ef1cce7abf9300efbf9cb8a98cf4711a8
Xrezo <141310481+Xrezo@users.noreply.github.com>
2024-07-05 12:08:44 +0800

    ok

---
+++ PPASBOOK/LOCATION/DistrictCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DistrictCollectionViewCell.swift
+//  PPASBOOK
+//
+//  Created by STDC_14 on 05/07/2024.
+//
+
+import UIKit
+
+class DistrictCollectionViewCell: UICollectionViewCell {
+    
+}
